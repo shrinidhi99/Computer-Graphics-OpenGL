25ec9cdc8552fd28509328617ac52e16b05fe2c4
shrinidhi99 <shrinidhi99.varna@gmail.com>
2020-01-09 15:24:28 +0530

    lab2 prog2

---
+++ lab2/cube-pyramid-simple.c
@@ -0,0 +1,120 @@
+#include <GL/glut.h>
+ 
+char title[] = "Pyramid and Cube";
+
+void initGL() {
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f); 
+    glClearDepth(1.0f);                   
+    glEnable(GL_DEPTH_TEST);   
+    glDepthFunc(GL_LEQUAL);    
+    glShadeModel(GL_SMOOTH);   
+    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);  
+}
+
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); 
+    glMatrixMode(GL_MODELVIEW); 
+
+    glLoadIdentity();
+    glTranslatef(1.5f, 0.0f, -7.0f); 
+ 
+    glBegin(GL_QUADS); 
+        glColor3f(0.0f, 1.0f, 0.0f);
+        glVertex3f( 1.0f, 1.0f, -1.0f);
+        glVertex3f(-1.0f, 1.0f, -1.0f);
+        glColor3f(1.0f, 0.5f, 0.0f);
+        glVertex3f(-1.0f, 1.0f,  1.0f);
+        glVertex3f( 1.0f, 1.0f,  1.0f);
+
+        glColor3f(1.0f, 0.5f, 0.0f); 
+        glVertex3f( 1.0f, -1.0f,  1.0f);
+        glVertex3f(-1.0f, -1.0f,  1.0f);
+        glColor3f(1.0f, 0.0f, 0.0f);
+        glVertex3f(-1.0f, -1.0f, -1.0f);
+        glVertex3f( 1.0f, -1.0f, -1.0f);
+
+        glColor3f(1.0f, 0.0f, 0.0f);
+        glVertex3f( 1.0f,  1.0f, 1.0f);
+        glVertex3f(-1.0f,  1.0f, 1.0f);
+        glColor3f(1.0f, 1.0f, 0.0f);
+        glVertex3f(-1.0f, -1.0f, 1.0f);
+        glVertex3f( 1.0f, -1.0f, 1.0f);
+
+
+        glColor3f(1.0f, 1.0f, 0.0f);
+        glVertex3f( 1.0f, -1.0f, -1.0f);
+        glColor3f(0.0f, 0.0f, 1.0f);
+        glVertex3f(-1.0f, -1.0f, -1.0f);
+        glVertex3f(-1.0f,  1.0f, -1.0f);
+        glVertex3f( 1.0f,  1.0f, -1.0f);
+
+        glColor3f(0.0f, 0.0f, 1.0f);
+        glVertex3f(-1.0f,  1.0f,  1.0f);
+        glVertex3f(-1.0f,  1.0f, -1.0f);
+        glColor3f(1.0f, 0.0f, 1.0f);
+        glVertex3f(-1.0f, -1.0f, -1.0f);
+        glVertex3f(-1.0f, -1.0f,  1.0f);
+
+        glColor3f(1.0f, 0.0f, 1.0f);
+        glVertex3f(1.0f,  1.0f, -1.0f);
+        glVertex3f(1.0f,  1.0f,  1.0f);
+        glColor3f(0.0f,1.0f,0.0f);
+        glVertex3f(1.0f, -1.0f,  1.0f);
+        glVertex3f(1.0f, -1.0f, -1.0f);
+    glEnd();
+ 
+    glLoadIdentity();
+    glTranslatef(-1.5f, 0.0f, -6.0f);
+ 
+    glBegin(GL_TRIANGLES);
+        // Front
+        glColor3f(1.0f, 0.0f, 0.0f);     // Red
+        glVertex3f( 0.0f, 1.0f, 0.0f);
+        glVertex3f(-1.0f, -1.0f, 1.0f);
+        glVertex3f(1.0f, -1.0f, 1.0f);
+
+        // Right
+        glVertex3f(0.0f, 1.0f, 0.0f);
+        glColor3f(0.0f, 0.0f, 1.0f);     // Blue
+        glVertex3f(1.0f, -1.0f, 1.0f);
+        glVertex3f(1.0f, -1.0f, -1.0f);
+
+        // Back
+        glVertex3f(0.0f, 1.0f, 0.0f);
+        glColor3f(0.0f, 1.0f, 0.0f);     // Green
+        glVertex3f(1.0f, -1.0f, -1.0f);
+        glVertex3f(-1.0f, -1.0f, -1.0f);
+
+        // Left
+        glColor3f(1.0f,0.0f,0.0f);       // Red
+        glVertex3f( 0.0f, 1.0f, 0.0f);
+        glVertex3f(-1.0f,-1.0f,-1.0f);
+        glVertex3f(-1.0f,-1.0f, 1.0f);
+    glEnd();
+    glutSwapBuffers();
+}
+ 
+void reshape(GLsizei width, GLsizei height) {  
+   
+    if (height == 0) height = 1;
+    GLfloat aspect = (GLfloat)width / (GLfloat)height;
+
+    glViewport(0, 0, width, height);
+
+    glMatrixMode(GL_PROJECTION);   
+    glLoadIdentity();              
+    gluPerspective(45.0f, aspect, 0.1f, 100.0f);
+}
+ 
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);            // Initialize GLUT
+    glutInitDisplayMode(GLUT_DOUBLE); // Enable double buffered mode
+    glutInitWindowSize(640, 480);   // Set the window's initial width & height
+    glutInitWindowPosition(50, 50); // Position the window's initial top-left corner
+    glutCreateWindow(title);          // Create window with the given title
+    glutDisplayFunc(display);       // Register callback handler for window re-paint event
+    glutReshapeFunc(reshape);       // Register callback handler for window re-size event
+    initGL();                       // Our own OpenGL initialization
+    glutMainLoop();                 // Enter the infinite event-processing loop
+    return 0;
+}
