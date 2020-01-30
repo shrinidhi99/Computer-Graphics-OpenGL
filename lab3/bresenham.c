7d72f975a84dce89f1e55131ed6764a4ae8d30a9
shrinidhi99 <shrinidhi99.varna@gmail.com>
2020-01-30 15:57:07 +0530

    lab4

--- lab3/bresenham.c
+++ lab3/bresenham.c
@@ -8,38 +8,38 @@ void display()
     glClear(GL_COLOR_BUFFER_BIT);
     int x1, x2, y1, y2;
     gluOrtho2D(-20, 20, -20, 20);
-    x1 = -15,  y1 = -10, x2 = 16, y2 = 15;
+    x1 = -15, y1 = -10, x2 = 16, y2 = 15;
     glBegin(GL_POINTS);
     // logic
-    int m_new = 2 * (y2 - y1); 
-    int slope_error_new = m_new - (x2 - x1); 
-    for (int x = x1, y = y1; x <= x2; x++) 
-    { 
-        // printf("( %d, %d)\n", x, y); 
-        glVertex2f(1*x, 1*y);
-        // Add slope to increment angle formed 
-        slope_error_new += m_new; 
+    int m_new = 2 * (y2 - y1);
+    int slope_error_new = m_new - (x2 - x1);
+    for (int x = x1, y = y1; x <= x2; x++)
+    {
+        // printf("( %d, %d)\n", x, y);
+        glVertex2f(1 * x, 1 * y);
+        // Add slope to increment angle formed
+        slope_error_new += m_new;
 
-        // Slope error reached limit, time to 
-        // increment y and update slope error. 
-        if (slope_error_new >= 0) 
-        { 
-            y++; 
-            slope_error_new  -= 2 * (x2 - x1); 
-        } 
+        // Slope error reached limit, time to
+        // increment y and update slope error.
+        if (slope_error_new >= 0)
+        {
+            y++;
+            slope_error_new -= 2 * (x2 - x1);
+        }
     }
     glEnd();
     glFlush();
 }
 int main(int argc, char **argv)
 {
-glutInit(&argc, argv);
-glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-glutInitWindowSize(1000, 800);
-glutInitWindowPosition(0, 0);
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(1000, 800);
+    glutInitWindowPosition(0, 0);
 
-// Giving name to window
-glutCreateWindow("Bresenham’s Line Generation Algorithm");
-glutDisplayFunc(display);
-glutMainLoop();
+    // Giving name to window
+    glutCreateWindow("Bresenham’s Line Generation Algorithm");
+    glutDisplayFunc(display);
+    glutMainLoop();
 }
--- others/bresenham-1.cpp
+++ others/bresenham-1.cpp
@@ -1,7 +1,7 @@
-#include<GL/glut.h>
-#include<math.h>
-#include<algorithm>
-#include<iostream>
+#include <GL/glut.h>
+#include <math.h>
+#include <algorithm>
+#include <iostream>
 
 using namespace std;
 void initGL()
@@ -12,24 +12,24 @@ void initGL()
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT);
-    glMatrixMode( GL_PROJECTION );
+    glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluOrtho2D( -500, 500.0, -500.0,500.0 );
-    int x1 = 0, y1 = 0, x2 = 20, y2 = 20;
+    gluOrtho2D(-500, 500.0, -500.0, 500.0);
+    int x1 = 0, y1 = 0, x2 = 100, y2 = 100;
 
     int dx = x2 - x1;
-    int dy = y2- y1;
+    int dy = y2 - y1;
 
     int stepx = 1, stepy = 1;
 
     int fraction = 0, fraction_increment = 0;
 
-    if(dy < 0)
+    if (dy < 0)
     {
         dy = -dy;
         stepy = -1;
     }
-    if(dx < 0)
+    if (dx < 0)
     {
         dx = -dx;
         stepx = -1;
@@ -37,33 +37,33 @@ void display()
 
     glBegin(GL_POINTS);
 
-    if(dx >= dy) {
-        fraction = 2*dy - dx;
-        fraction_increment = 2*dy;
-        while(x1 <= x2)
+    if (dx >= dy)
+    {
+        fraction = 2 * dy - dx;
+        fraction_increment = 2 * dy;
+        while (x1 <= x2)
         {
             x1 += stepx;
-            if(fraction >= 0)
+            if (fraction >= 0)
             {
                 y1 += stepy;
-                fraction -= 2*dx; 
+                fraction -= 2 * dx;
             }
 
             fraction += fraction_increment;
             glVertex2i(x1, y1);
         }
-
-    } 
-    else 
+    }
+    else
     {
-        fraction = 2*dx - dy;
-        fraction_increment = 2*dx;
-        while(y1 <= y2)
+        fraction = 2 * dx - dy;
+        fraction_increment = 2 * dx;
+        while (y1 <= y2)
         {
-            if(fraction >= 0) 
+            if (fraction >= 0)
             {
                 x1 += stepx;
-                fraction -= 2*dy;
+                fraction -= 2 * dy;
             }
             fraction += fraction_increment;
             glVertex2i(x1, y1);
@@ -72,16 +72,14 @@ void display()
 
     glEnd();
     glFlush();
-    
-
 }
 
-int main(int argc, char **argv) 
+int main(int argc, char **argv)
 {
     glutInit(&argc, argv);
-    glutCreateWindow("DDA");
+    glutCreateWindow("Bresenham");
     glutInitWindowPosition(0, 0);
-    glutInitWindowSize(320, 320);
+    glutInitWindowSize(640, 640);
     glutDisplayFunc(display);
     initGL();
     glutMainLoop();
